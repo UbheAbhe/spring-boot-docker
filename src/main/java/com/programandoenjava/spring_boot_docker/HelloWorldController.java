7a64154ecac1974a449b565cb8fcf2c8d835344e
UbheAbhe <omoranmore@gmail.com>
2025-11-14 12:34:10 -0500

    Saludo

--- src/main/java/com/programandoenjava/spring_boot_docker/HelloWorldController.java
+++ src/main/java/com/programandoenjava/spring_boot_docker/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping
     public String helloWorld() {
-        return "Hello World!";
+        return "Hello World! Orestes Antonio Moran More, hola profesor Ponte";
     }
 }
